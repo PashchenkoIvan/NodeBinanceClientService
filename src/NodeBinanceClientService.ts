e2e4772c6584f45252293e9345d3bf37d721048e
PashchenkoIvan <ipaschenko09@gmail.com>
2024-08-21 13:54:53 +0300

    fixex constructor

--- src/NodeBinanceClientService.ts
+++ src/NodeBinanceClientService.ts
@@ -1,19 +1,16 @@
 import Binance from "binance-api-node"
 
 class NodeBinanceClientService {
-    private binance_api_key: string
-    private binance_secret_key: string
+
+    private binance_client: Binance
 
     constructor(binance_api_key: string, binance_secret_key: string) {
-        this.binance_api_key = binance_api_key
-        this.binance_secret_key = binance_secret_key
+        this.binance_client = Binance({
+            apiKey: binance_api_key,
+            apiSecret: binance_secret_key,
+            getTime: () => new Date().getTime()
+        })
     }
-
-    private client = Binance({
-        apiKey: this.binance_api_key,
-        apiSecret: this.binance_secret_key,
-        getTime: () => new Date().getTime()
-    })
 }
 
 module.exports = NodeBinanceClientService
